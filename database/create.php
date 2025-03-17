8e0df68af44b6e50a0b69c25764fc35c0ae8f118
TechMWhiz <chelseamorona49@gmail.com>
2025-03-17 23:52:42 +0800

    adding add modal and fixing the create.php having error

--- database/create.php
+++ database/create.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('database.php'); 
+require_once 'database.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $title = $_POST['title'];
@@ -9,17 +9,37 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $year_released = $_POST['year_released'];
     $description = $_POST['description'];
 
+    if (empty($title) || empty($genre) || empty($ratings) || empty($year_released) || empty($description)) {
+        $_SESSION['status'] = "Please fill out all fields!";
+        header('Location: ../index.php');
+        exit();
+    }
 
-    $sql = "INSERT INTO movies (title, year_released, genre, ratings, description) VALUES ('$title', '$genre', '$ratings', '$year_released', '$description')";
+    $title = mysqli_real_escape_string($conn, $title);
+    $genre = mysqli_real_escape_string($conn, $genre);
+    $ratings = mysqli_real_escape_string($conn, $ratings);
+    $year_released = mysqli_real_escape_string($conn, $year_released);
+    $description = mysqli_real_escape_string($conn, $description);
 
-    if (mysqli_query($conn,$sql)) {
-        $_SESSION['status'] = "created";
+    $check_sql = "SELECT * FROM movies WHERE title = '$title' AND year_released = '$year_released'";
+    $result = mysqli_query($conn, $check_sql);
+
+    if (!$result) {
+        $_SESSION['status'] = "SQL Error: " . mysqli_error($conn);
+    } elseif (mysqli_num_rows($result) > 0) {
+        $_SESSION['status'] = "Movie already exists!";
     } else {
-        $_SESSION['status'] = "error: " . $stmt->error;
+        // Movie doesn't exist proceed with adding Mae is here
+        $sql = "INSERT INTO movies (title, genre, ratings, year_released, description) VALUES ('$title', '$genre', '$ratings', '$year_released', '$description')";
+        if (mysqli_query($conn, $sql)) {
+            $_SESSION['status'] = "Movie added successfully!";
+        } else {
+            $_SESSION['status'] = "SQL Error: " . mysqli_error($conn);
+        }
     }
 
     mysqli_close($conn);
-    header('Location: index.php');
+    header('Location: ../index.php');
     exit();
 }
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
+ session_start();
+  include('database/database.php');
   include('partials\header.php');
   include('partials\sidebar.php');
-  include('database\database.php');
 
   // Your PHP BACK CODE HERE
+  $sql = "SELECT * FROM movies";
+  $movies = $conn->query($sql);
 
 ?>
 
@@ -31,7 +34,7 @@
                   <h5 class="card-title">Default Table</h5>
                 </div>
                 <div>
-                  <button class="btn btn-primary btn-sm mt-4 mx-3">Add Employee</button>
+                  <button class="btn btn-primary btn-sm mt-4 mx-3" data-bs-toggle="modal" data-bs-target="#add">Add Employee</button>
                 </div>
               </div>
 
@@ -40,29 +43,19 @@
                 <thead>
                   <tr>
                     <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Position</th>
-                    <th scope="col">Age</th>
-                    <th scope="col">Address</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Genre</th>
+                    <th scope="col">Ratings</th>
+                    <th scope="col">Year_released	</th>
+                    <th scope="col">Description</th>
                     <th scope="col" class="text-center">Action</th>
                   </tr>
                 </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Brandon Jacob</td>
-                    <td>Designer</td>
-                    <td>28</td>
-                    <td>2016-05-25</td>
-                    <td class="d-flex justify-content-center">
-                      <button class="btn btn-success btn-sm mx-1">Edit</button>
-                      <button class="btn btn-primary btn-sm mx-1" title="View Employee Information" data-bs-toggle="modal" data-bs-target="#editInfo">View</button>
-                      <button class="btn btn-danger btn-sm mx-1">Delete</button>
-                    </td>
-                  </tr>
+                <tbody>   
                 </tbody>
               </table>
               <!-- End Default Table Example -->
+
             </div>
             <div class="mx-4">
               <nav aria-label="Page navigation example">
@@ -80,6 +73,51 @@
         </div>
 
         
+      </div>
+
+
+      <!-- Add Modal &Mae is here -->
+      <div class="modal fade" id="add" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addLabel" aria-hidden="true">
+        <div class = "modal-dialog modal-dialog-cenetered">
+          <form action="database/create.php" method="POST">
+            <div class = "modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="addLabel">Add New Movie</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+             <div class="modal-body">
+               <div class = "container">
+                  <div class="row">
+                    <div class="col-12 mt-2">
+                      <label for="title">Title</label>
+                      <input type="text" name="title" id="title" class="form-control" placeholder="Enter Title">
+                    </div>
+                    <div class="col-12 mt-2">
+                      <label for="genre">Genre</label>
+                      <input type="text" name="genre" id="genre" class="form-control" placeholder="Enter Genre">
+                    </div>
+                    <div class="col-8 mt-2">
+                      <label for="ratings">Ratings</label>
+                      <input type="text" name="ratings" id="ratings" class="form-control" placeholder="Enter Ratings">
+                    </div>
+                    <div class="col-8 mt-2">
+                      <label for="year_released">Year_released</label>
+                      <input type="text" name="year_released" id="year_released" class="form-control" placeholder="Enter Year_released">
+                    </div>
+                    <div class="col-12 mt-2">
+                      <label for="description">Description</label>
+                      <input type="text" name="description" id="description" class="form-control" placeholder="Enter Description">
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-primary">Add</button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
 
       <!-- Modal -->
